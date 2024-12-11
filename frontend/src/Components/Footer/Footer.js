4f8a45a8386507c8de7b16e0c6deb421e8eedbb8
denisa122 <neagudenisa12@yahoo.com>
2024-12-11 14:15:42 +0100

    initial footer page

---
+++ frontend/src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Insights = () => {
+    return (
+        <footer>
+            This is the footer
+        </footer>
+    );
+};
+
+export default Footer;
