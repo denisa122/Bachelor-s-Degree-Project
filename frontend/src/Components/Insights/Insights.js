2fda5bff2775de786ffca97d3455dccda5fbb20b
denisa122 <neagudenisa12@yahoo.com>
2024-12-12 22:43:41 +0100

    header on mood tracker + insights pages

--- frontend/src/Components/Insights/Insights.js
+++ frontend/src/Components/Insights/Insights.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './Insights.css';
+import "./Insights.css";
 
-import Navigation from '../Navigation/Navigation';
+// Icons & images
+import Logo from "../../assets/logo.png";
+
+import Navigation from "../Navigation/Navigation";
 
 const Insights = () => {
-    return (
-        <div className='flex flex-row'>
-            <Navigation />
-            This is the insights page
+  return (
+    <div className="flex flex-row">
+      <Navigation />
+      <div className="insightsPage">
+        <div className="headerInsightsPage">
+          <img src={Logo} alt="Logo" className="logoJournal"></img>
+          <h1 className="text-3xl mb-1 mt-2">Your Insights</h1>
+          <h2 className="italic text-lg">
+            Uncover patterns, gain clarity, and understand your emotional
+            journey
+          </h2>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Insights;
--- frontend/src/Components/MoodTracker/MoodTracker.js
+++ frontend/src/Components/MoodTracker/MoodTracker.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './MoodTracker.css';
+import "./MoodTracker.css";
 
-import Navigation from '../Navigation/Navigation';
+// Icons & images
+import Logo from "../../assets/logo.png";
+
+import Navigation from "../Navigation/Navigation";
 
 const MoodTracker = () => {
-    return (
-        <div className='flex flex-row'>
-            <Navigation />
-            This is the mood tracker page
+  return (
+    <div className="flex flex-row">
+      <Navigation />
+      <div className="moodTrackerPage">
+        <div className="headerMoodTrackerPage">
+          <img src={Logo} alt="Logo" className="logoJournal"></img>
+          <h1 className="text-3xl mb-1 mt-2">Your Mood Tracker</h1>
+          <h2 className="italic text-lg">
+            Reflect, set goals and track your emotional journey throughout the
+            day
+          </h2>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MoodTracker;
