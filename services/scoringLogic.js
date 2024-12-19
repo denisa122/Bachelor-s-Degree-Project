25a3357bb91264b2889f0e1672d476e98251d92d
denisa122 <neagudenisa12@yahoo.com>
2024-12-19 23:37:15 +0100

    score calculations for other questionnaires

--- services/scoringLogic.js
+++ services/scoringLogic.js
@@ -44,6 +44,70 @@ const calculateScores = (responses) => {
         if (answer === "Yes") moodScore += 1;
         if (answer === "No") moodScore -= 1;
         break;
+      // Midday questionnaire
+      case "44":
+        moodScore = 0;
+        energyLevel = 0;
+        stressLevel = 0;
+        moodScore += parseInt(answer) || 0;
+        break;
+      case "45":
+        if (answer === "Yes") moodScore -= 1;
+        if (answer === "No") moodScore += 1;
+        break;
+      case "46":
+      case "47":
+      case "48":
+      case "49":
+        if (answer === "Yes") moodScore -= 1;
+        if (answer === "No") moodScore += 1;
+        break;
+      case "50":
+      case "51":
+      case "52":
+        energyLevel += parseInt(answer) || 0;
+        break;
+      case "53":
+        if (answer === "Yes") stressLevel -= 1;
+        if (answer === "No") stressLevel += 1;
+        break;
+      case "54":
+        if (answer === "Work") stressLevel -= 1;
+        if (answer === "Studies") stressLevel -= 1;
+        if (answer === "Family") stressLevel -= 1;
+        if (answer === "friends") stressLevel -= 1;
+        break;
+      case "55":
+        if (answer === "Excited") moodScore += 1;
+        if (answer === "Neutral") moodScore += 0;
+        if (answer === "Anxious") moodScore -= 1;
+        break;
+      // Evening questionnaire
+      case "56":
+        moodScore = 0;
+        energyLevel = 0;
+        stressLevel = 0;
+        moodScore += parseInt(answer) || 0;
+        break;
+      case "57":
+        energyLevel += parseInt(answer) || 0;
+        break;
+      case "58":
+        if (answer === "Yes") stressLevel += 1;
+        if (answer === "No") stressLevel += 0;
+        break;
+      case "59":
+        if (answer === "Yes") moodScore -= 1;
+        if (answer === "No") moodScore += 0;
+        break;
+      case "60":
+        if (answer === "Yes") moodScore += 1;
+        if (answer === "No") moodScore -= 1;
+        break;
+      case "61":
+      case "62":
+      case "63":
+      case "64":
       default:
         break;
     }
@@ -53,5 +117,5 @@ const calculateScores = (responses) => {
 };
 
 module.exports = {
-    calculateScores,
+  calculateScores,
 };
