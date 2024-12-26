96e2da8b517c3b802172c0bfb0aa1fc5f849176b
denisa122 <neagudenisa12@yahoo.com>
2024-12-26 12:30:59 +0100

    all associations

--- db/postgresql/models/index.js
+++ db/postgresql/models/index.js
@@ -7,16 +7,40 @@ const MoodResponse = require("./moodResponse");
 const Question = require("./question");
 const Questionnaire = require("./questionnaire");
 const User = require("./user");
+const QuestionnaireSubmission = require("./questionnaireSubmission");
 
 // Associations
-Questionnaire.hasMany(Question, {foreignKey: "questionnaireID", onDelete: "CASCADE"});
-Question.belongsTo(Questionnaire, {foreignKey: "questionnaireID", onDelete: "CASCADE"});
-MoodEntry.belongsTo(User, {foreignKey: 'userID', onDelete: 'CASCADE'});
-MoodResponse.belongsTo(MoodEntry, {foreignKey: 'entryID', onDelete: 'CASCADE'});
-MoodResponse.belongsTo(Question, {foreignKey: 'questionID', onDelete: 'CASCADE'});
+// User - Goal
 Goal.belongsTo(User, {foreignKey: 'userID', onDelete: 'CASCADE'});
+User.hasMany(Goal, {foreignKey: 'userID', onDelete: 'CASCADE'});
+
+// User - JournalMetadata
 JournalMetadata.belongsTo(User, {foreignKey: 'userID', onDelete: 'CASCADE'});
-// Add more later
+User.hasMany(JournalMetadata, {foreignKey: 'userID', onDelete: 'CASCADE'});
+
+// User - MoodEntry 
+MoodEntry.belongsTo(User, {foreignKey: 'userID', onDelete: 'CASCADE'});
+User.hasMany(MoodEntry, {foreignKey: 'userID', onDelete: 'CASCADE'});
+
+// MoodEntry - MoodResponse
+MoodResponse.belongsTo(MoodEntry, {foreignKey: 'entryID', onDelete: 'CASCADE'});
+MoodEntry.hasMany(MoodResponse, {foreignKey: 'entryID', onDelete: 'CASCADE'});
+
+// Question - MoodResponse
+MoodResponse.belongsTo(Question, {foreignKey: 'questionID', onDelete: 'CASCADE'});
+Question.hasMany(MoodResponse, {foreignKey: 'questionID', onDelete: 'CASCADE'});
+
+// Questionnaire - Question
+Question.belongsTo(Questionnaire, {foreignKey: "questionnaireID", onDelete: "CASCADE"});
+Questionnaire.hasMany(Question, {foreignKey: "questionnaireID", onDelete: "CASCADE"});
+
+// User - QuestionnaireSubmission
+QuestionnaireSubmission.belongsTo(User, {foreignKey: "userID", onDelete: "CASCADE"});
+User.hasMany(QuestionnaireSubmission, {foreignKey: "userID", onDelete: "CASCADE"});
+
+// Questionnaire - QuestionnaireSubmission
+QuestionnaireSubmission.belongsTo(Questionnaire, {foreignKey: "questionnaireID", onDelete: "CASCADE"});
+Questionnaire.hasMany(QuestionnaireSubmission, {foreignKey: "questionnaireID", onDelete: "CASCADE"});
 
 module.exports = {
     sequelize,
