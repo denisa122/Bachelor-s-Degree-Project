573f0ca2e1377983c3c87d6426299f6ff1394310
denisa122 <neagudenisa12@yahoo.com>
2024-12-30 13:15:21 +0100

    insert data to render postgreSQL db

--- db/postgresql/insertData.js
+++ db/postgresql/insertData.js
@@ -1,25 +1,22 @@
-require ('dotenv').config();
-const fs = require('fs');
-const { Pool } = require('pg');
+require("dotenv").config();
+const fs = require("fs");
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.POSTGRESQL_DB_USER,
-    host: process.env.POSTGRESQL_DB_HOST,
-    database: process.env.POSTGRESQL_DB_NAME,
-    password: process.env.POSTGRESQL_DB_PASSWORD,
-    port: process.env.POSTGRESQL_DB_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 const seedDatabase = async () => {
-    try {
-        const seedSQL = fs.readFileSync('./db/postgresql/seed_data.sql', 'utf8');
-        await pool.query(seedSQL);
-        console.log('Data seeded successfully');
-    } catch (error) {
-        console.error('Error seeding data', error);
-    } finally {
-        pool.end();
-    }
+  try {
+    const seedSQL = fs.readFileSync("./db/postgresql/seed_data.sql", "utf8");
+    await pool.query(seedSQL);
+    console.log("Data seeded successfully");
+  } catch (error) {
+    console.error("Error seeding data", error);
+  } finally {
+    pool.end();
+  }
 };
 
 seedDatabase();
